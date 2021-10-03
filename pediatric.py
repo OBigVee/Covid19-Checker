1e0745aef4dd4d6b4bac37ae52b9689cb791016e
OBigVee <adedejivictor98@gmail.com>
2021-10-03 15:18:40 +0100

    json

--- pediatric.py
+++ pediatric.py
@@ -1,85 +1,104 @@
-Q5_PED = "Q5-PED. What sex were you assigned at birth, on your original birth certificate?\n\
-            a. Male\n\
-            b. Female\n\
-            c. I prefer not to say\n\
-            d. I don’t know\n\
-            " 
+Q5_PED = {"text": "What sex were you assigned at birth, on your original birth certificate?",
+          "options": {
+              "a": "Male",
+              "b": "Female",
+              "c": "I prefer not to say",
+              "d": "I don’t know"
+          }
+          }
 
-Q35_PED = "How do you currently describe yourself?\n\
-        a. Male\n\
-        b. Female\n\
-        c. Transgender\n\
-        d. I prefer not to say\n\
-    "
-Q37_PED = "What is your race ?\n\
-                a. white\n\
-                b.Black or African American\n\
-                c. American Indian or Alaska Native\n\
-                d.Asian\n\
-                e. Native Hawaiian or Other Pacific Islander\n\
-                f. I prefer not to say\n\
-    "
-Q1_PED = """
-            Do you have any of these life-threatening symptoms?\n
-                • Pale, gray, or blue-colored skin, lips, or nail beds, depending on skin tone\n
-                • Severe and constant pain or pressure in the chest\n
+Q35_PED = {"text": "How do you currently describe yourself?",
+           "options": {
+               "a": "Male",
+               "b": "Female",
+               "c": "Transgender",
+               "d": "I prefer not to say"
+           }}
+
+Q37_PED = {"text": "What is your race ?",
+           "options": {
+               "a": "white",
+               "b": "Black or African American",
+               "c": "American Indian or Alaska Native",
+               "d": "Asian",
+               "e": "Native Hawaiian or Other Pacific Islander",
+               "f": "I prefer not to say"
+           }}
+
+Q1_PED = {"text": """
+            Do you have any of these life-threatening symptoms?
+                • Pale, gray, or blue-colored skin, lips, or nail beds, depending on skin tone
+                • Severe and constant pain or pressure in the chest
                 • Difficulty breathing (such as gasping for air, being unable to walk or talk without catching your breath, severe wheezing,\
-                    nostrils flaring, grunting,or ribs or stomach moving in and out deeply and rapidly as you breathe)\n
-                • Disoriented (acting confused or very irritable)\n
-                • Unconscious or very difficult to wake up\n
-                • New or worsening seizures\n
-                • Signs of low blood pressure (too weak to stand, dizziness, lightheaded,feeling cold, pale, clammy skin)\n
-                • Dehydration (dry lips and mouth, not urinating much, sunken eyes) \n 
-                • Refusing to drink liquids \n
-                • Frequent vomiting \n
-            """
-Q3_PED = "Are you feeling Sick ?"
+                    nostrils flaring, grunting,or ribs or stomach moving in and out deeply and rapidly as you breathe)
+                • Disoriented (acting confused or very irritable)
+                • Unconscious or very difficult to wake up
+                • New or worsening seizures
+                • Signs of low blood pressure (too weak to stand, dizziness, lightheaded,feeling cold, pale, clammy skin)
+                • Dehydration (dry lips and mouth, not urinating much, sunken eyes) 
+                • Refusing to drink liquids 
+                • Frequent vomiting 
+            """,
+          "options": {
+              "a": "Yes",
+              "b": "No"}
+          }
 
-Q6_PED = """In the two weeks before you felt sick, have you been in close contact with someone who has COVID-19? —excluding people who have had COVID-19\ 
-                within the past 3 months.\n
-                You have been in close contact if you have\n
+Q3_PED = {"text": "Are you feeling Sick ?",
+          "options": {
+              "a": "Yes",
+              "b": "No"
+          }}
+
+Q6_PED = {"text": """In the two weeks before you felt sick, have you been in close contact with someone who has COVID-19? —excluding people who have had COVID-19\ 
+                within the past 3 months.
+                You have been in close contact if you have
                 1. been within 6 feet of someone who has COVID-19 for a combined total of 15 minutes or more
                 over a 24 hour period or \n
-                2. provided care at home to someone who is sick with COVID-19 or\n
-                3. had direct physical contact (hugged or kissed) with someone who has COVID-19 or\n
-                4. shared eating or drinking utensils with someone who has COVID-19 or\n
-                5. been sneezed on or coughed on by someone who has COVID-19\n
-                
-                a. Yes\n
-                b. No\n 
-                c. I Don't Know \n
-            """
+                2. provided care at home to someone who is sick with COVID-19 or
+                3. had direct physical contact (hugged or kissed) with someone who has COVID-19 or
+                4. shared eating or drinking utensils with someone who has COVID-19 or
+                5. been sneezed on or coughed on by someone who has COVID-19""",
 
-Q31_PED = """In the last 10 day, have you tested positive for the coronavirus that causes COVID-19?\n
-                a.Yes, tested positive\n
-                b.No, tested negative\n
-                c.No, waiting for results\n
-                d.No, not tested\n
-            """
+          "options": {
+              "a": "Yes",
+              "b": "No",
+              "c": "I Don't Know"}}
 
-Q7_PED = """ 
-    In the last 10 days, have you experienced any of the symptoms listed below? (check all that apply)\n
-            a. Fever or feeling feverish (such as chills, sweating)\n
-            b. Cough\n
-            c. Mild or moderate difficulty breathing (breathing slightly faster than normal, feeling like you can’t inhale or exhale, or wheezing,\
-                especially during exhaling or breathing out)\n
-            d. Sore throat\n
-            e. Muscle aches or body aches\n
-            f. Headache\n
-            g. Diarrhea\n
-            h. Nausea or vomiting\n
-            i. Stomach ache or pain in abdomen\n
-            j. New loss of taste or smell\n
-            k. New rash\n
-            l. Red eyes\n
-            m. Congestion or runny nose\n
-            n. Other symptoms\n
-    """
-Q8_PED = "PED: Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center, \
-        juvenile detention center, or homeless shelter)?"
 
-Q9_PED = "n the last two weeks, have you attended or spent time in a group setting (for example school, dormitory, child care,\
-         sporting event)?"
+Q31_PED = {"text": "In the last 10 day, have you tested positive for the coronavirus that causes COVID-19?",
+           "options": {
+               "a": "Yes, tested positive",
+               "b": "No, tested negative",
+               "c": "No, waiting for results",
+               "d": "No, not tested"
+           }}
+
+Q7_PED = {"text":
+          " In the last 10 days, have you experienced any of the symptoms listed below? (check all that apply)",
+          "options": {
+              "a": "Fever or feeling feverish (such as chills, sweating)",
+              "b": "Cough",
+              "c": "Mild or moderate difficulty breathing (breathing slightly faster than normal, feeling like you can’t inhale or exhale, or wheezing,\
+                especially during exhaling or breathing out)",
+              "d": "Sore throat",
+              "e": "Muscle aches or body aches",
+              "f": "Headache",
+              "g": "Diarrhea",
+              "h": "Nausea or vomiting",
+              "i": "Stomach ache or pain in abdomen",
+              "j": "New loss of taste or smell",
+              "k": "New rash",
+              "l": "Red eyes",
+              "m": "Congestion or runny nose",
+              "n": "Other symptoms",
+          }
+          }
+Q8_PED = {"text": "Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center, \
+        juvenile detention center, or homeless shelter)?"}
+
+Q9_PED = {"text": "In the last two weeks, have you attended or spent time in a group setting (for example school, dormitory, child care,\
+         sporting event)?"}
 
 
 Q10_PED = """ 
@@ -101,11 +120,11 @@ Q10_PED = """
             n. Down syndrome or Down’s syndrome\n
         """
 
-Q11_PED = "Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center,\
-             juvenile detention center, or homeless shelter)?"
+Q11_PED = {"text": "Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center,\
+             juvenile detention center, or homeless shelter)?"}
 
-Q12_PED = "In the last two weeks, have you attended or spent time in a group setting (for example school, dormitory, child care, \
-            sporting)?"
+Q12_PED = {"text": "In the last two weeks, have you attended or spent time in a group setting (for example school, dormitory, child care, \
+            sporting)?"}
 
 Q13_PED = """Do any of these apply to you? (check any)\n
                 a. Lung disease, such as moderate to severe asthma or cystic fibrosis\n
@@ -145,11 +164,11 @@ Q14_PED = """
         n. Other symptoms\n
 """
 
-Q15_PED = "Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center, \
-            juvenile detention center, or homeless shelter)?\n"
+Q15_PED = {"text": "Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center, \
+            juvenile detention center, or homeless shelter)?"}
 
-Q16_PED = "In the last two weeks, have you attended or spent time in a group setting (for example school, dormitory, child care, \
-            sporting event)?"
+Q16_PED = {"text": "In the last two weeks, have you attended or spent time in a group setting (for example school, dormitory, child care, \
+            sporting event)?"}
 Q17_PED = """ Do any of these apply to you? (check any)\n
                 a. Lung disease, such as moderate to severe asthma or cystic fibrosis\n
                 b. Born premature\n
@@ -167,11 +186,11 @@ Q17_PED = """ Do any of these apply to you? (check any)\n
                 m. None of the above\n
                 n. Down syndrome or Down’s syndrome\n
 """
-Q18_PED = "Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center,\
-     juvenile detention center, or homeless shelter)?\n"
+Q18_PED = {"text": "Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center,\
+     juvenile detention center, or homeless shelter)?"}
 
-Q19_PED = "In the last two weeks, have you attended or spent time in a group setting (for example school,dormitory, child care, sporting\
-             event)?\n"
+Q19_PED = {"text": "In the last two weeks, have you attended or spent time in a group setting (for example school,dormitory, child care, sporting\
+             event)?"}
 
 Q20_PED = """Do any of these apply to you? (check any)\n
                 a. Lung disease, such as moderate to severe asthma or cystic fibrosis\n
@@ -191,23 +210,23 @@ Q20_PED = """Do any of these apply to you? (check any)\n
                 n. Down syndrome or Down’s syndrome\n
     """
 
-Q25_PED = """
-    In the last two weeks, have you been in close contact with someone who has COVID-19? —excluding people who have had COVID-19 within the past 3 months.\n
-        You have been in close contact if you have\n
-            1. been within 6 feet of someone who has COVID-19 for a combined total of 15 minutes or more over a 24 hour period or\n
-            2. provided care at home to someone who is sick with COVID-19 or \n
-            3. had direct physical contact (hugged or kissed) with someone who has COVID-19 or\n
-            4. shared eating or drinking utensils with someone who has COVID-19 or\n
-            5. been sneezed on or coughed on by someone who has COVID-19\n\
+Q25_PED = {"text":
+           """In the last two weeks, have you been in close contact with someone who has COVID-19? —excluding people who have had COVID-19 within the past 3 months.\n
+                    You have been in close contact if you have
+                        1. been within 6 feet of someone who has COVID-19 for a combined total of 15 minutes or more over a 24 hour period or\n
+                        2. provided care at home to someone who is sick with COVID-19 or 
+                        3. had direct physical contact (hugged or kissed) with someone who has COVID-19 or
+                        4. shared eating or drinking utensils with someone who has COVID-19 or
+                        5. been sneezed on or coughed on by someone who has COVID-19""",
+           "options": {
+               "a": "Yes",
+               "b": "No",
+               "c": "I Don't Know",
+           }}
 
-            a. Yes \n\
-            b. No \n\  
-            c. I Don't Know \n\    
-"""
+Q26_PED = {"text": "Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center,\
+         juvenile detention center, or homeless shelter)?"}
 
-Q26_PED ="Do you live in a group home or other setting with others (pediatric skilled nursing facility, behavioral health center,\
-         juvenile detention center, or homeless shelter)?"
-
-Q27_PED = "In the last two weeks, have you attended or spent time in a group setting (for example school, dormitory, child care,\
-             sporting event)?\n"
+Q27_PED = {"text": "In the last two weeks, have you attended or spent time in a group setting (for example school, dormitory, child care,\
+             sporting event)?"}
 Q28_PED = ""
